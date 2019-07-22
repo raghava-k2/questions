6393e51ebbb40010c1f9b28c69190da5c6761d1a
vijaya raghava k <raghava.k2@gmail.com>
2019-07-22 20:16:34 +0530

    removed slider code from body component

--- static/src/components/body/body.js
+++ static/src/components/body/body.js
@@ -3,7 +3,6 @@ import { Container, Row, Col } from 'react-bootstrap';
 import QuestionsList from '../question/questionList';
 import TableTop from '../rank/tableTop';
 import './body.css'
-import { Slider, SliderHeader, SliderBody, SliderFooter } from '../slider';
 export default class Body extends Component {
     constructor(props) {
         super(props);
@@ -28,22 +27,6 @@ export default class Body extends Component {
                         </section>
                     </Col>
                 </Row>
-                <button type="button" onClick={this.showManageColumns}>open</button>
-                <Slider show={this.state.showSlider} onClose={e => { this.setState({ showSlider: false }) }}>
-                    <SliderHeader>
-                        <h1>Manage Columns</h1>
-                    </SliderHeader>
-                    <SliderBody>
-                        <ul>
-                            {Array(50).fill(0).map((item, index) => (
-                                <li key={index}>{`value${index}`}</li>
-                            ))}
-                        </ul>
-                    </SliderBody>
-                    <SliderFooter>
-                        <button>Close</button>
-                    </SliderFooter>
-                </Slider>
             </Container>
         );
     }
