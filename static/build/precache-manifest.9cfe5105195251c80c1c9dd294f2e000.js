f14a4eb814b359244d49d5bb3281cedfa079d25b
vijaya raghava k <raghava.k2@gmail.com>
2019-08-11 12:45:22 +0530

    Add build files to git repo

---
+++ static/build/precache-manifest.9cfe5105195251c80c1c9dd294f2e000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86b3bbef81df0044c62d",
+    "url": "/static/js/main.53919fea.chunk.js"
+  },
+  {
+    "revision": "e587c91e7897b01d5e64",
+    "url": "/static/js/2.51482cd9.chunk.js"
+  },
+  {
+    "revision": "86b3bbef81df0044c62d",
+    "url": "/static/css/main.7633a8af.chunk.css"
+  },
+  {
+    "revision": "e587c91e7897b01d5e64",
+    "url": "/static/css/2.96c6fe41.chunk.css"
+  },
+  {
+    "revision": "407fedc9ab08656b0d8d70028cd48a9e",
+    "url": "/index.html"
+  }
+];
